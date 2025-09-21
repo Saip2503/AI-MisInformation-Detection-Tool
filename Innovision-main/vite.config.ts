58e831d4cf1cf9b011040e9849117a92630960f7
Sai809701 <112499446+Sai809701@users.noreply.github.com>
2025-09-21 14:15:32 +0530

    some changes

--- Innovision-main/vite.config.ts
+++ Innovision-main/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -15,5 +16,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/AI-MisInformation-Detection-Tool/", 
+  base: new URL(pkg.homepage).pathname, 
 }));
